7604eb505a5232e52ead8b666662efc11965e04d
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2022-11-11 20:56:08 -0500

    animation

--- src/Link.js
+++ src/Link.js
@@ -1,14 +1,55 @@
 import React from "react";
 
 class Link extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            textlerp: 0
+        };
+    }
+
+    mouseEnter = () => {
+        if (this.increment) {
+            clearInterval(this.increment);
+        }
+        
+        this.increment = setInterval(() => {
+            this.setState({textlerp: this.state.textlerp + 0.1});
+
+            if (this.state.textlerp >= 1) {
+                clearInterval(this.increment);
+            }
+        }, 1);
+    }
+
+    mouseExit = () => {
+        if (this.increment) {
+            clearInterval(this.increment);
+        }
+
+        this.increment = setInterval(() => {
+            this.setState({textlerp: this.state.textlerp - 0.1});
+
+            if (this.state.textlerp <= 0) {
+                clearInterval(this.increment);
+            }
+        }, 1);
+    }
+
     render() {
         return (
-            <a href={this.props.href} className={this.props.className}>
+            <a href={this.props.href} className={this.props.className} onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseExit}>
                 <i className={`${this.props.icon} icon`}></i> 
-                {this.props.name} 
-                <i className="fa-solid fa-arrow-up-right-from-square external"></i>
+                <span className="expansion">
+                    <span className="text">{(this.props.name).slice(0, this.props.name.length * Math.max(this.state.textlerp, 0))}</span>
+                    <i className="fa-solid fa-arrow-up-right-from-square external"></i>
+                </span>
             </a>
         );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.increment);
     }
 }
 
